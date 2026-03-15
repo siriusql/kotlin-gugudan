2f2b057dd5f448ad1125b251536b6ba7a4a3edd8
siriusql <siriuseo@nate.com>
2026-03-15 22:28:38 +0900

    init: register an initial source

---
+++ src/main/kotlin/org/example/kotlingugudan/KotlinGugudanApplication.kt
@@ -0,0 +1,11 @@
+package org.example.kotlingugudan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinGugudanApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinGugudanApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/kotlingugudan/KotlinGugudanApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.kotlingugudan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinGugudanApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
