043e7f56a7d3b768747b626816179179191d372f
siriusql <siriuseo@nate.com>
2026-03-17 23:47:16 +0900

    test: add gugudan test

--- src/main/kotlin/org/example/kotlingugudan/controller/GugudanController.kt
+++ src/main/kotlin/org/example/kotlingugudan/controller/GugudanController.kt
@@ -1,16 +1,23 @@
 package org.example.kotlingugudan.controller
 
 import org.example.kotlingugudan.service.GugudanService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/gugudan")
 class GugudanController (
     private val gugudanService: GugudanService
 ){
 
-    @GetMapping("/gugudan/{dan}")
-    fun gugudan(@PathVariable dan: Int) =
-        gugudanService.gugudan(dan)
+    @GetMapping("/{dan}")
+    fun gugudan(@PathVariable dan: Int) : ResponseEntity<List<String>>{
+
+        var result:List<String> = gugudanService.gugudan(dan)
+
+        return ResponseEntity.ok(result)
+    }
 }
--- src/test/kotlin/org/example/kotlingugudan/service/GugudanServiceTest.kt
+++ src/test/kotlin/org/example/kotlingugudan/service/GugudanServiceTest.kt
@@ -1,18 +1,24 @@
 package org.example.kotlingugudan.service
 
 import org.junit.jupiter.api.Test
+import io.github.oshai.kotlinlogging.KotlinLogging
 
 class GugudanServiceTest {
 
     private var gugudanService = GugudanService()
+    private var log = KotlinLogging.logger {  }
 
     @Test
     fun `3단계 테스트`() {
-        val result = gugudanService.gugudan(3)
 
-        for(i in result){
-            println(i)
+        var dan:Int=3
+        val items:List<String> = gugudanService.gugudan(dan)
+
+        for(item in items){
+            log.info(item)
         }
+
+        log.info("done gugudan")
 
     }
 }
